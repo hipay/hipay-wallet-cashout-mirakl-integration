901ce594b8d829440aa715d184bb5af6d540e7ba
Ivanis KOUAME <ivanis.kouame@smile.fr>
2015-12-24 16:04:58 +0100

    Add commands skeleton

---
+++ src/Command/Cashout/GenerateCommand.php
@@ -0,0 +1,14 @@
+<?php
+namespace Hipay\Command\Cashout;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * File ProcessCommand.php
+ *
+ * @author    Ivanis Kouamé <ivanis.kouame@smile.fr>
+ * @copyright 2015 Smile
+ */
+class GenerateCommand extends Command
+{
+
+}
---
+++ src/Command/Vendor/ProcessCommand.php
@@ -0,0 +1,14 @@
+<?php
+namespace Hipay\Command\Vendor;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * File ProcessCommand.php
+ *
+ * @author    Ivanis Kouamé <ivanis.kouame@smile.fr>
+ * @copyright 2015 Smile
+ */
+class ProcessCommand extends Command
+{
+
+}
