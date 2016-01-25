e02c14147b4d1e3a30a17b8f27bcd6e372813294
Ivanis KOUAME <ivanis.kouame@smile.fr>
2016-01-25 15:22:12 +0100

    Add Hello world to verify installation

--- web/index.php
+++ web/index.php
@@ -9,12 +9,15 @@
 use Symfony\Component\HttpFoundation\Request;
 const ROOT_PATH = __DIR__;
 
-require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/bootstrap.php';
 
 $app = new Silex\Application();
 $app->post('/',function (Request $request) use ($app, $notificationHandler) {
     $notificationHandler->handleHipayNotification($request->get('xml'));
+});
+
+$app->get('/', function () {
+   return 'Hello World';
 });
 
 $app->run();
