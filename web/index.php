e03ffbb9814bce61679ce8162d2d9b3dbf7d7a83
Ivanis KOUAME <ivanis.kouame@smile.fr>
2016-02-04 16:23:16 +0100

    Add error handling

--- web/index.php
+++ web/index.php
@@ -6,15 +6,23 @@
  * @copyright 2015 Smile
  */
 
-use Symfony\Component\HttpFoundation\Request;
 const ROOT_PATH = __DIR__;
 
 require_once __DIR__ . '/../app/bootstrap.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 $app->post('/', function (Request $request) use ($app, $notificationHandler) {
     $notificationHandler->handleHipayNotification($request->getContent());
-    return 'Notification handled';
+    return new Response(null, 204);
+});
+
+
+$app->error(function (Exception $e, $code) use ($app, $notificationHandler) {
+    $notificationHandler->handleException($e);
+    return new Response($e->getMessage());
 });
 
 $app->get('/', function () {
