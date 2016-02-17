683f6185996092f3fe7066300a2d82c136050228
Jonathan Tiret <jtiret@hipay.com>
2016-02-17 11:02:02 +0100

    Fixes issues

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get update && apt-get install -y \
 	ftp \
 	libssl-dev \
 	mysql-client \
+	phpunit \
     git
 
 # pdo_mysql
--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-while !((ping -c1 -W1 db) && (ping -c1 -W1 ftp) && (ping -c1 -W1 smtp) && (ping -c1 -W1 getcomposer.org)); do sleep 3; done
+#!/bin/sh
 
 # Install composer
 curl -sS https://getcomposer.org/installer | php -- --filename=composer -- --install-dir=/usr/local/bin
