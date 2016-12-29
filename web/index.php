b357d912aa24d6797311610798cd90a3fa4099e2
Johan Protin <jprotin@hipay.com>
2016-12-29 11:33:24 +0100

    Add new notification when the API response send an error KYC validation

--- web/index.php
+++ web/index.php
@@ -10,11 +10,25 @@ require_once __DIR__ . '/../app/bootstrap.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use HiPay\Wallet\Mirakl\Integration\Parameter\Accessor;
 
 $app = new Silex\Application();
 
 $app->post('/{anyplace}', function (Request $request) use ($app, $notificationHandler) {
-    $notificationHandler->handleHipayNotification(rawurldecode($request->request->get('xml')));
+    //Get the parameters
+    $parameters = new Accessor(__DIR__ . "/../config/parameters.yml");
+    $swiftTransport = new Swift_SmtpTransport(
+        $parameters['mail.host'],
+        $parameters['mail.port'],
+        $parameters['mail.security']
+    );
+    if (isset($parameters['mail.username']) && isset($parameters['mail.password'])) {
+        $swiftTransport->setUsername($parameters['mail.username']);
+        $swiftTransport->setPassword($parameters['mail.password']);
+    }
+    $mailer = new Swift_Mailer($swiftTransport);
+    $mailer_message = new Swift_Message();
+    $notificationHandler->handleHipayNotification(rawurldecode($request->request->get('xml')), $parameters, $mailer, $mailer_message);
     return new Response(null, 204);
 })->assert("anyplace", ".*");
 
