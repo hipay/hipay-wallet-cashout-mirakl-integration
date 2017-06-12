7206ee022d06cfa17e2223a04f1289450bf33e4e
Johan Protin <jprotin@hipay.com>
2017-06-12 17:20:20 +0200

    dev dashboard with demo mode

--- web/index.php
+++ web/index.php
@@ -53,7 +53,26 @@ $app->get('/', function() use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect($app["url_generator"]->generate("login"));
     }
-    return $app['twig']->render('pages/vendors.twig');
+    $message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+    $message .= 'Suspendisse rutrum et mauris euismod faucibus.';
+    $message .= 'Vivamus eu sapien lacus. ';
+    $message .= 'Sed sit amet nunc efficitur risus bibendum bibendum.';
+    $message .= 'Pellentesque eu lectus sodales, pharetra libero id, condimentum urna.';
+    $rows = array(
+        array('mirakl_id'=>'2789','message'=>$message, 'class'=>'danger', 'status'=>'Non créé', 'hipay_id'=>'-', 'document_sent'=>'-',),
+        array('mirakl_id'=>'2788','message'=>'', 'class'=>'', 'status'=>'Identifié', 'hipay_id'=>'55999', 'document_sent'=>'5',),
+        array('mirakl_id'=>'2787','message'=>'', 'class'=>'warning', 'status'=>'Non identifié', 'hipay_id'=>'55998', 'document_sent'=>'3',),
+        array('mirakl_id'=>'2786','message'=>'', 'class'=>'success', 'status'=>'Créé', 'hipay_id'=>'55997', 'document_sent'=>'5',),
+        array('mirakl_id'=>'2785','message'=>'', 'class'=>'', 'status'=>'Identifié', 'hipay_id'=>'55996', 'document_sent'=>'4',),
+        array('mirakl_id'=>'2784','message'=>'', 'class'=>'warning', 'status'=>'Non identifié', 'hipay_id'=>'55995', 'document_sent'=>'3',),
+        array('mirakl_id'=>'2783','message'=>'', 'class'=>'warning', 'status'=>'Non identifié', 'hipay_id'=>'55994', 'document_sent'=>'4',),
+        array('mirakl_id'=>'2782','message'=>'', 'class'=>'', 'status'=>'Identifié', 'hipay_id'=>'55993', 'document_sent'=>'4',),
+        array('mirakl_id'=>'2781','message'=>$message, 'class'=>'danger', 'status'=>'Non créé', 'hipay_id'=>'-', 'document_sent'=>'-',),
+        array('mirakl_id'=>'2780','message'=>'', 'class'=>'warning', 'status'=>'Non identifié', 'hipay_id'=>'55992', 'document_sent'=>'5',),
+        array('mirakl_id'=>'2779','message'=>'', 'class'=>'', 'status'=>'Identifié', 'hipay_id'=>'55991', 'document_sent'=>'5',),
+    );
+
+    return $app['twig']->render('pages/vendors.twig', array('rows' => $rows));
 
 })->bind('vendors');
 
@@ -61,7 +80,27 @@ $app->get('/transferts', function() use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect($app["url_generator"]->generate("login"));
     }
-    return $app['twig']->render('pages/transferts.twig');
+
+    $message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+    $message .= 'Suspendisse rutrum et mauris euismod faucibus.';
+    $message .= 'Vivamus eu sapien lacus. ';
+    $message .= 'Sed sit amet nunc efficitur risus bibendum bibendum.';
+    $message .= 'Pellentesque eu lectus sodales, pharetra libero id, condimentum urna.';
+    $rows = array(
+        array('mirakl_id'=>'2789','transfert'=>'KO','withdrawal'=>'-','amount'=>'129,99€','balance'=>'0€','message'=>$message, 'class'=>'danger', 'status'=>'Non créé', 'hipay_id'=>'55888',),
+        array('mirakl_id'=>'2788','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'', 'status'=>'Identifié', 'hipay_id'=>'55999',),
+        array('mirakl_id'=>'2787','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'warning', 'status'=>'Non identifié', 'hipay_id'=>'55998',),
+        array('mirakl_id'=>'2786','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'success', 'status'=>'Créé', 'hipay_id'=>'55997',),
+        array('mirakl_id'=>'2785','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'', 'status'=>'Identifié', 'hipay_id'=>'55996',),
+        array('mirakl_id'=>'2784','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'warning', 'status'=>'Non identifié', 'hipay_id'=>'55995',),
+        array('mirakl_id'=>'2783','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'warning', 'status'=>'Non identifié', 'hipay_id'=>'55994',),
+        array('mirakl_id'=>'2782','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'', 'status'=>'Identifié', 'hipay_id'=>'55993',),
+        array('mirakl_id'=>'2781','transfert'=>'OK','withdrawal'=>'KO','amount'=>'129,99€','balance'=>'20129,99€','message'=>$message, 'class'=>'danger', 'status'=>'Non créé', 'hipay_id'=>'55993',),
+        array('mirakl_id'=>'2780','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'warning', 'status'=>'Non identifié', 'hipay_id'=>'55992',),
+        array('mirakl_id'=>'2779','transfert'=>'OK','withdrawal'=>'OK','amount'=>'129,99€','balance'=>'20129,99€','message'=>'', 'class'=>'', 'status'=>'Identifié', 'hipay_id'=>'55991',),
+    );
+
+    return $app['twig']->render('pages/transferts.twig', array('rows' => $rows));
 
 })->bind('transferts');
 
@@ -69,7 +108,28 @@ $app->get('/logs', function() use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect($app["url_generator"]->generate("login"));
     }
-    return $app['twig']->render('pages/logs.twig');
+
+    $message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+    $message .= 'Suspendisse rutrum et mauris euismod faucibus.';
+    $message .= 'Vivamus eu sapien lacus. ';
+    $message .= 'Sed sit amet nunc efficitur risus bibendum bibendum.';
+    $message .= 'Pellentesque eu lectus sodales, pharetra libero id, condimentum urna.';
+    $rows =
+        array(
+            array('date'=>'27/03/2017 12:43:23','type_info'=>'WARNING','error_message'=>"Error Bank info is empty",'action'=>'Création Wallet','mirakl_id'=>'44356','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'27/03/2017 12:43:23','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44355','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'26/03/2017 12:43:23','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44354','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'26/03/2017 12:43:23','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44353','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'26/03/2017 12:43:23','type_info'=>'WARNING','error_message'=>"Error Bank info is empty",'action'=>'Création Wallet','mirakl_id'=>'44356','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'25/03/2017 12:50:23','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44351','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'25/03/2017 12:32:21','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44350','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'25/03/2017 12:30:53','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44349','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'24/03/2017 16:45:33','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44348','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'24/03/2017 16:43:23','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44347','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+            array('date'=>'24/03/2017 16:40:13','type_info'=>'INFO','error_message'=>"",'action'=>'Création Wallet','mirakl_id'=>'44346','message'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id vehicula eros, vel mollis dui. Pellentesque sapien sem, pulvinar vel massa non, varius venenatis nibh. Cras eget commodo augue, posuere dictum purus.',),
+        );
+
+    return $app['twig']->render('pages/logs.twig', array('rows' => $rows));
 
 })->bind('logs');
 
@@ -154,7 +214,8 @@ $app->match('/login', function(Request $request) use ($app) {
 
         if ($data['ws_login'] === $parameters['hipay.wsLogin'] && $data['ws_password'] === $parameters['hipay.wsPassword']) {
             $app['session']->set('user', array('username' => $parameters['hipay.wsLogin']));
-            return $app['twig']->render('pages/vendors.twig');
+            //return $app['twig']->render('pages/vendors.twig');
+            return $app->redirect($app["url_generator"]->generate("vendors"));
         }
 
         $sent = true;
