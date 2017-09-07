66b180bbd1d5141903db54e63618dd2034d72e5e
BERTHELOT AYMERIC <lemaildeaymeric@gmail.com>
2017-09-07 10:35:59 +0200

    Prepare deploy / docker machine

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,9 @@ COLOR_SUCCESS='\033[0;32m'
 NC='\033[0m'
 
 ls
-/docker/scripts/ci.sh
+/var/www/html/docker/scripts/ci.sh
 
-/docker/scripts/setup.sh
+/var/www/html/docker/scripts/setup.sh
 
 chmod 777 -Rf /var/www/html/var
 
