08065ac58dc841b937c1bc3b26625a1591f219bf
Etienne Landais <elandais@hipay.com>
2017-09-27 17:42:08 +0200

    add new command to recover logs from past vendors

---
+++ src/Command/Log/LogVendorsRecoverCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace HiPay\Wallet\Mirakl\Integration\Command\Log;
+
+use DateTime;
+use HiPay\Wallet\Mirakl\Integration\Command\AbstractCommand;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use HiPay\Wallet\Mirakl\Integration\Entity\Batch;
+use HiPay\Wallet\Mirakl\Integration\Entity\LogVendors;
+use HiPay\Wallet\Mirakl\Integration\Entity\BatchRepository;
+use HiPay\Wallet\Mirakl\Integration\Entity\VendorRepository;
+use HiPay\Wallet\Mirakl\Integration\Entity\LogVendorsRepository;
+use HiPay\Wallet\Mirakl\Vendor\Processor as VendorProcessor;
+use HiPay\Wallet\Mirakl\Notification\Model\LogVendorsInterface;
+
+/**
+ * File LogVendorsRecoverCommand.php
+ *
+ */
+class LogVendorsRecoverCommand extends AbstractCommand
+{
+    protected $batchManager;
+    protected $vendorManager;
+    protected $logVendorsManager;
+
+    public function __construct(LoggerInterface $logger, VendorProcessor $processor, BatchRepository $batchManager,
+                                VendorRepository $vendorManager, LogVendorsRepository $logVendorsManager)
+    {
+        $this->batchManager      = $batchManager;
+        $this->vendorManager     = $vendorManager;
+        $this->logVendorsManager = $logVendorsManager;
+
+        parent::__construct($logger);
+    }
+
+    protected function configure()
+    {
+        $this->setName('logs:vendors:recover')
+            ->setDescription('Recover Logs vendors from vendors');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $batch = new Batch($this->getName());
+        $this->batchManager->save($batch);
+
+        try {
+            $this->processVendors();
+            $batch->setEndedAt(new \DateTime());
+            $this->batchManager->save($batch);
+        } catch (\Exception $e) {
+
+            $batch->setError($e->getMessage());
+            $this->batchManager->save($batch);
+
+            $this->logger->critical($e->getMessage());
+        }
+    }
+
+    private function processVendors()
+    {
+        $vendors = $this->vendorManager->findAll();
+
+        $logs = array();
+
+        foreach ($vendors as $vendor) {
+            if (!$this->logVendorsManager->findByMiraklId($vendor->getMiraklId())) {
+                $log = new LogVendors();
+                $log->setMiraklId($vendor->getMiraklId());
+                $log->setHipayId($vendor->getHipayId());
+                $log->setStatus(LogVendorsInterface::SUCCESS);
+                if ($vendor->getHipayIdentified()) {
+                    $log->setStatusWalletAccount(LogVendorsInterface::WALLET_IDENTIFIED);
+                } else {
+                    $log->setStatusWalletAccount(LogVendorsInterface::WALLET_NOT_IDENTIFIED);
+                }
+                $log->setDate(DateTime::createFromFormat('Y-m-d', '2017-09-04'));  // set default date to dashboard release
+                $logs[] = $log;
+
+                $this->logger->info("create log from vendor ".$vendor->getMiraklId()." (Mirakl Id)");
+            }
+        }
+
+        $this->logVendorsManager->saveAll($logs);
+    }
+}
