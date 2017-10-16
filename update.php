f57e93c5663c56c367b64dc9cfc597611c01772a
Etienne Landais <elandais@hipay.com>
2017-10-16 15:42:39 +0200

    change update process

--- update.php
+++ update.php
@@ -15,7 +15,7 @@ system('tar czvf  '.__DIR__.'/backup.tar.gz '.__DIR__.'/ ', $status);
 echo '<div class="alert alert-dismissible alert-info">updating app, this may take a while </div>';
 
 putenv('COMPOSER_HOME='.__DIR__.'/vendor/bin/composer');
- system('composer config --global github-oauth.github.com 2692ed08b94c436069f03eebda7e36e59ad10cfc', $status);
+
 system('yes | composer create-project hipay/hipay-wallet-cashout-mirakl-integration '.__DIR__.'/update dev-feature/EC-122  2>&1', $status);
 
 system('chmod 755 -R '.__DIR__, $status);
