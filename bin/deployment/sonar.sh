87ffbf1a92aea8f24094a0bb9e8e82f6b29231e6
Etienne Landais <elandais@hipay.com>
2017-11-14 11:40:48 +0100

    add sonar support

--- bin/deployment/sonar.sh
+++ bin/deployment/sonar.sh
@@ -1,9 +1,6 @@
 if [ -z "$CI_PULL_REQUESTS" ];then
     echo "Not a pull request"
 elif [ $CI_PULL_REQUESTS != "" ];then
-    echo $CI_PULL_REQUESTS
-    echo $CI_PULL_REQUEST
-    echo $CIRCLE_PR_NUMBER
     PR_NUMBER=$(echo $CI_PULL_REQUEST | cut -d'/' -f 7)
     sonar-scanner -Dsonar.host.url=$SONAR_URL -Dsonar.login=$SONAR_LOGIN -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$PR_NUMBER -Dsonar.github.oauth=$GITHUB_TOKEN
 fi
