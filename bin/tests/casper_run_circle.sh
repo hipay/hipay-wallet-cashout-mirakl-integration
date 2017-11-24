993c4e30d004ed753e02da2a1d0817d413733df8
Etienne Landais <elandais@hipay.com>
2017-11-24 10:47:28 +0100

    add two simple casper tests

--- bin/tests/casper_run_circle.sh
+++ bin/tests/casper_run_circle.sh
@@ -8,4 +8,4 @@ header="bin/tests/"
 
 
 echo "Execute casperjs tests"
-casperjs test ${header}000*/*/*/*.js ${header}000*/000[0-2]*.js ${header}0[0-1][0-9]*/[0-1]*/[0-9][0-3][0-9][0-9]-*.js --url=$BASE_URL/ --login-backend=$MIRAKL_CONNECTOR_HIPAY_WS_LOGIN --pass-backend=$MIRAKL_CONNECTOR_HIPAY_WS_PASSWORD --ignore-ssl-errors=true --ssl-protocol=any
+casperjs test ${header}000*/000[0-2]*.js ${header}0[0-1][0-9]*/[0-1]*/[0-9][0-3][0-9][0-9]-*.js --url=$BASE_URL/ --login-backend=$MIRAKL_CONNECTOR_HIPAY_WS_LOGIN --pass-backend=$MIRAKL_CONNECTOR_HIPAY_WS_PASSWORD --ignore-ssl-errors=true --ssl-protocol=any
