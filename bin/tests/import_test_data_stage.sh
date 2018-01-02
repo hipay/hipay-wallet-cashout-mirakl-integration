adda59ae24f245ce1f05860118b7942e9a2db361
Etienne Landais <elandais@hipay.com>
2018-01-02 11:28:25 +0100

    Add vendors page tests

--- bin/tests/import_test_data_stage.sh
+++ bin/tests/import_test_data_stage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+cd /var/www/html
 php bin/console dbal:import bin/tests/Data/import_log_vendors.sql
 php bin/console dbal:import bin/tests/Data/import_log_operations.sql
 php bin/console dbal:import bin/tests/Data/import_logs.sql
