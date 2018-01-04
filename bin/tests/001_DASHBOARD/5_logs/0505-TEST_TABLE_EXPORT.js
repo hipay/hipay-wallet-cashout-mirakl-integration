45c496e60aa8f44f78974dea57245b1d6df5704b
Etienne Landais <elandais@hipay.com>
2018-01-04 12:02:30 +0100

    Fixing tests bug on circleCI

--- bin/tests/001_DASHBOARD/5_logs/0505-TEST_TABLE_EXPORT.js
+++ bin/tests/001_DASHBOARD/5_logs/0505-TEST_TABLE_EXPORT.js
@@ -48,12 +48,7 @@ casper.test.begin('Test logs table export', function (test) {
             table.checkDownloadFile(test, 'logs.csv', '#export-action', 'Date,Level,Action,Mirakl ID,Message\n2017-12-01 15:40:13,DEBUG,,,\"Test message\"\n2017-12-01 15:40:13,DEBUG,,,\"Test message\"\n2017-12-01 15:40:13,INFO,Wallet creation,,\"Test message\"\n2017-12-01 15:40:14,INFO,Wallet creation,,\"Test message\"\n2017-12-01 15:40:14,INFO,Wallet creation,,\"Test message\"\n2017-12-01 15:40:14,INFO,Wallet creation,,\"Test message\"\n2017-12-01 15:40:15,INFO,Wallet creation,1,\"Test message\"\n2017-12-01 15:40:15,INFO,Wallet creation,5,\"Test message\"\n2017-12-01 15:40:15,NOTICE,Wallet creation,6,\"Test message\"\n2017-12-01 15:40:16,WARNING,Wallet creation,4,\"Test message\"\n2017-12-01 15:40:16,ERROR,Wallet creation,139,\"Test message\"\n2017-12-01 15:40:16,CRITICAL,Wallet creation,118,\"Test message\"\n2017-12-01 15:40:16,ALERT,Wallet creation,102,\"Test message\"\n2017-12-01 15:40:16,EMERGENCY,Wallet creation,137,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,104,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,107,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,132,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,150,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,123,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,119,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,124,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,112,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,106,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,131,\"Test message\"\n2017-12-01 15:40:16,INFO,Wallet creation,144,\"Test message\"\n2017-12-05 15:40:17,INFO,Wallet creation,120,\"Test message\"\n2017-12-05 15:40:18,INFO,Wallet creation,114,\"Test message\"\n2017-12-05 15:40:19,INFO,Wallet creation,113,\"Test message\"\n2017-12-05 15:40:20,CRITICAL,Wallet creation,127,\"Test message\"\n2017-12-05 15:40:21,INFO,Wallet creation,128,\"Test message\"');
         })
         // Going back to logs page
-        .thenOpen(baseURL + '/dashboard/logs')
-        // waiting for table to be loaded
-        .then(function () {
-            // check if logs table is displayed
-            table.checkFirstPage(test, 'table_logs');
-        })
+        .back()
         // checking with filter
         .then(function () {
 
@@ -72,12 +67,7 @@ casper.test.begin('Test logs table export', function (test) {
             table.checkDownloadFile(test, 'logs.csv', '#export-action', 'Date,Level,Action,Mirakl ID,Message\n2017-12-05 15:40:17,INFO,Wallet creation,120,\"Test message\"\n2017-12-05 15:40:18,INFO,Wallet creation,114,\"Test message\"\n2017-12-05 15:40:19,INFO,Wallet creation,113,\"Test message\"\n2017-12-05 15:40:21,INFO,Wallet creation,128,\"Test message\"');
         })
         // Going back to logs page
-        .thenOpen(baseURL + '/dashboard/logs')
-        // waiting for table to be loaded
-        .then(function () {
-            // check if logs table is displayed
-            table.checkFirstPage(test, 'table_logs');
-        })
+        .back()
         // checking empty results
         .then(function () {
 
