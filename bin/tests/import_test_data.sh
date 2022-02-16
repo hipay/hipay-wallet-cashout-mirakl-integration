b46224d367e7488a0c6945817f7afc28bf03b9ce
Etienne LANDAIS <elandais@hipay.com>
2022-02-16 16:37:35 +0000

    replace deprecated runners

--- bin/tests/import_test_data.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -p $1 -f docker-compose.test.yml exec -T web php bin/console dbal:import bin/tests/Data/import_log_vendors.sql
-docker-compose -p $1 -f docker-compose.test.yml exec -T web php bin/console dbal:import bin/tests/Data/import_vendors.sql
-docker-compose -p $1 -f docker-compose.test.yml exec -T web php bin/console dbal:import bin/tests/Data/import_log_operations.sql
-docker-compose -p $1 -f docker-compose.test.yml exec -T web php bin/console dbal:import bin/tests/Data/import_logs.sql
